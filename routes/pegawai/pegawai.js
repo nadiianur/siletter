ca5c38a5f7514f39274e7380172a704baba9a76d
najlahmaira <najlahumairadesni@gmail.com>
2024-06-23 10:09:10 +0700

    Doneee

---
+++ routes/pegawai/pegawai.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const controllers = require('../../controllers/pegawai/pegawai')
+const middleware = require('../../middleware/authentication')
+
+router.post('/tambahPegawai', controllers.tambahPegawai)
+router.post('/loginPegawai', controllers.loginPegawai)
+router.delete('/logoutPegawai', middleware.verifyTokenPegawai, controllers.logoutPegawai)
+router.post('/forgotPass', controllers.forgotPassword)
+router.post('/changePassPegawai/:id_pegawai', controllers.confirmPassword)
+router.get('/dataProfile', middleware.verifyTokenPegawai, controllers.detailAkun)
+
+module.exports = router
